4ff996af430903b014afd75e1cbcdef3ed78e321
Gonçalo Fontenele <goncalogfb@poli.ufrj.br>
2020-08-05 10:23:33 -0300

    Aula 2 NLW

---
+++ README.md
@@ -0,0 +1,17 @@
+# Server
+
+# Functionalities
+
+## Conections
+
+- Route to list total accomplished connections;
+- Route to ceate a new connection;
+
+
+## Lessons
+
+- Route to Create a Lesson;
+- Route to list all Lessons;
+    -Filter over subject, day and hour;
+
+
---
+++ src/routes.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
+
+const routes = express.Router();
+const classesController = new ClassesController();
+const connectionsController = new ConnectionsController();
+
+// Corpo da requisição: (Request Body) : Dados
+// Route Params: Identificar qual recurso se deseja atualizar ou deletar 
+// Query Params: Paginação, Filtros, Ordenação
+
+routes.get('/classes', classesController.index);
+routes.post('/classes', classesController.create);
+
+routes.post('/connections', connectionsController.create)
+
+export default routes;
