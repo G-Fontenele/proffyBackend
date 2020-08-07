d12a86864503ac90df04baa0a13dfa52040dce5c
Gonçalo Fontenele <goncalogfb@poli.ufrj.br>
2020-08-07 13:56:52 -0300

    Added config folder to server

---
+++ src/config/index.ts
@@ -0,0 +1,6 @@
+export const url_server = 'https://zeus.herokuapp.com';
+
+export const url_local = 'http://localhost:3333';
+
+export var port = process.env.PORT || 3333;
+
--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,13 @@ import cors from 'cors';
 
 import routes from './routes';
 
+import {url_local, url_server, port} from './config';
+
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(port);
